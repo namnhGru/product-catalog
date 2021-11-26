4645bcc8c6a9bf6b9a6fcdce19110a2fceb22cf1
namnhGru <hoangnamsdc@gmail.com>
2021-11-26 13:30:37 +0700

    fix minor bug that cause stock state not flip

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -15,7 +15,7 @@ class ProductTable extends React.Component {
                                 ? products.filter(product => product.name.includes(this.props.filteredText))
                                 : products
         let afterStockFilter = this.props.onlyInStock
-                                ? afterTextFilter.filter(product => product.stock === true)
+                                ? afterTextFilter.filter(product => product.stocked === true)
                                 : afterTextFilter 
         return afterStockFilter
     }
